d63aa44c35448bedaf57f237a390423b336cfd0b
Sanpi <sanpi@homecomputing.fr>
2017-04-01 18:12:43 +0200

    Create Application struct

--- src/main.rs
+++ src/main.rs
@@ -50,38 +50,102 @@ impl Redpitaya {
     }
 }
 
+pub struct EventLoop {
+    ui_needs_update: bool,
+    last_update: std::time::Instant,
+}
+
+impl EventLoop {
+    pub fn new() -> Self {
+        EventLoop {
+            last_update: std::time::Instant::now(),
+            ui_needs_update: true,
+        }
+    }
+
+    pub fn next(&mut self, display: &glium::Display) -> Vec<glium::glutin::Event> {
+        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
+        // since the last yield.
+        let last_update = self.last_update;
+        let sixteen_ms = std::time::Duration::from_millis(16);
+        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
+        if duration_since_last_update < sixteen_ms {
+            std::thread::sleep(sixteen_ms - duration_since_last_update);
+        }
+
+        // Collect all pending events.
+        let mut events = Vec::new();
+        events.extend(display.poll_events());
+
+        // If there are no events and the `Ui` does not need updating, wait for the next event.
+        if events.is_empty() && !self.ui_needs_update {
+            events.extend(display.wait_events().next());
+        }
+
+        self.ui_needs_update = false;
+        self.last_update = std::time::Instant::now();
+
+        events
+    }
+
+    pub fn needs_update(&mut self) {
+        self.ui_needs_update = true;
+    }
+}
+
+struct Application {
+}
+
+impl Application {
+    pub fn new() -> Application {
+        Application {
+        }
+    }
+
+    pub fn run(&self) {
+        let display = glium::glutin::WindowBuilder::new()
+            .with_title("Redpitaya")
+            .build_glium()
+            .unwrap();
+
+        let mut ui = conrod::UiBuilder::new([400.0, 200.0])
+            .build();
+
+        let mut renderer = conrod::backend::glium::Renderer::new(&display)
+            .unwrap();
+
+        let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
+
+        let mut event_loop = EventLoop::new();
+        'main: loop {
+            for event in event_loop.next(&display) {
+                if let Some(event) = conrod::backend::winit::convert(event.clone(), &display) {
+                    ui.handle_event(event);
+                    event_loop.needs_update();
+                }
+
+                match event {
+                    glium::glutin::Event::Closed => break 'main,
+                    _ => {},
+                }
+            }
+
+            if let Some(primitives) = ui.draw_if_changed() {
+                renderer.fill(&display, primitives, &image_map);
+                let mut target = display.draw();
+                target.clear_color(0.0, 0.0, 0.0, 1.0);
+                renderer.draw(&display, &mut target, &image_map)
+                    .unwrap();
+                target.finish()
+                    .unwrap();
+            }
+        }
+    }
+}
+
 fn main() {
     let redpitaya = Redpitaya::new("192.168.1.5", 5000);
 
-    let display = glium::glutin::WindowBuilder::new()
-        .with_title("Redpitaya")
-        .build_glium()
-        .unwrap();
-
-    let mut ui = conrod::UiBuilder::new([400.0, 200.0])
-        .build();
-
-    let mut renderer = conrod::backend::glium::Renderer::new(&display)
-        .unwrap();
-
-    let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
-
-    'main: loop {
-        for event in display.poll_events() {
-            match event {
-                glium::glutin::Event::Closed => break 'main,
-                _ => {},
-            }
-        }
-
-        if let Some(primitives) = ui.draw_if_changed() {
-            renderer.fill(&display, primitives, &image_map);
-            let mut target = display.draw();
-            target.clear_color(0.0, 0.0, 0.0, 1.0);
-            renderer.draw(&display, &mut target, &image_map)
-                .unwrap();
-            target.finish()
-                .unwrap();
-        }
-    }
+    Application::new()
+        .run();
 }
