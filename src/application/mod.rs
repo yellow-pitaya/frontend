413ad3a3fef501205ff87e76f42df2c96c41de59
Sanpi <sanpi@homecomputing.fr>
2017-04-03 17:10:51 +0200

    Draw grid

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -6,11 +6,15 @@ mod event;
 
 widget_ids! {
     struct Ids {
-        canvas,
+        master,
+        main_panel,
+        side_panel,
         toggle_oscillo,
         toggle_generator,
         toggle_generator_img,
+        scales,
         plot,
+        lines[],
     }
 }
 
@@ -20,6 +24,8 @@ pub struct Application {
     tx: ::std::sync::mpsc::Sender<String>,
     rx: ::std::sync::mpsc::Receiver<String>,
     bg_color: ::conrod::color::Color,
+    width: f64,
+    height: f64,
 }
 
 impl Application {
@@ -30,6 +36,8 @@ impl Application {
             tx: tx,
             rx: rx,
             bg_color: ::conrod::color::rgb(0.2, 0.35, 0.45),
+            width: 400.0,
+            height: 200.0,
         }
     }
 
@@ -39,13 +47,14 @@ impl Application {
             .build_glium()
             .unwrap();
 
-        let mut ui = ::conrod::UiBuilder::new([400.0, 200.0])
+        let mut ui = ::conrod::UiBuilder::new([self.width, self.height])
             .build();
 
         ui.fonts.insert_from_file("assets/fonts/NotoSans/NotoSans-Regular.ttf")
             .unwrap();
 
-        let ids = Ids::new(ui.widget_id_generator());
+        let mut ids = Ids::new(ui.widget_id_generator());
+        ids.lines.resize(20, &mut ui.widget_id_generator());
 
         let mut renderer = ::conrod::backend::glium::Renderer::new(&display)
             .unwrap();
@@ -84,13 +93,24 @@ impl Application {
     }
 
     fn set_widgets(&mut self, ref mut ui: ::conrod::UiCell, ids: &Ids) {
-        ::conrod::widget::Canvas::new()
-            .pad(30.0)
-            .color(self.bg_color)
-            .set(ids.canvas, ui);
+        let main_panel = ::conrod::widget::Canvas::new();
+        let side_panel = ::conrod::widget::Canvas::new()
+            .length(200.0);
 
-        self.oscillo_run_button(ui, ids);
-        self.generator_sin_button(ui, ids);
+        ::conrod::widget::Canvas::new()
+            .flow_right(&[
+                (ids.main_panel, main_panel),
+                (ids.side_panel, side_panel),
+            ])
+            .color(self.bg_color)
+            .set(ids.master, ui);
+
+        self.main_panel(ui, ids);
+        self.side_panel(ui, ids);
+    }
+
+    fn main_panel(&mut self, ui: &mut ::conrod::UiCell, ids: &Ids) {
+        self.draw_scales(ui, ids);
 
         if self.oscillo_started {
             self.tx.send("oscillo/data".into());
@@ -105,6 +125,65 @@ impl Application {
         }
     }
 
+    fn draw_scales(&mut self, ui: &mut ::conrod::UiCell, ids: &Ids) {
+        ::conrod::widget::Canvas::new()
+            .border(1.0)
+            .border_color(self.bg_color.invert())
+            .wh_of(ids.main_panel)
+            .top_left_of(ids.main_panel)
+            .border(1.0)
+            .set(ids.scales, ui);
+
+        let canvas = ui.rect_of(ids.scales)
+            .unwrap();
+
+        for i in 1..10 {
+            let x = canvas.x.start + (canvas.x.end - canvas.x.start) / 10.0 * (i as f64);
+
+            let mut color = ::conrod::color::WHITE;
+
+            if i != 5 {
+                color = color.alpha(0.1);
+            }
+
+            ::conrod::widget::Line::new(
+                [x, canvas.y.start],
+                [x, canvas.y.end]
+            )
+                .color(color)
+                .bottom_left_of(ids.scales)
+                .set(ids.lines[i], ui);
+
+            let y = canvas.y.start + (canvas.y.end - canvas.y.start) / 10.0 * (i as f64);
+
+            ::conrod::widget::Line::new(
+                [canvas.x.start, y],
+                [canvas.x.end, y]
+            )
+                .color(color)
+                .bottom_left_of(ids.scales)
+                .set(ids.lines[i + 9], ui);
+        }
+    }
+
+    fn draw_data<I>(&mut self, data: I, ui: &mut ::conrod::UiCell, ids: &Ids) where I: Iterator<Item=f64> {
+        let data: Vec<f64> = data.collect();
+
+        let plot = ::conrod::widget::PlotPath::new(0, data.len() - 1, -2.0, 2.0, |x| {
+            return data[x];
+        });
+
+        plot.color(::conrod::color::LIGHT_BLUE)
+            .top_left_of(ids.scales)
+            .wh_of(ids.scales)
+            .set(ids.plot, ui);
+    }
+
+    fn side_panel(&mut self, ui: &mut ::conrod::UiCell, ids: &Ids) {
+        self.oscillo_run_button(ui, ids);
+        self.generator_sin_button(ui, ids);
+    }
+
     fn oscillo_run_button(&mut self, ui: &mut ::conrod::UiCell, ids: &Ids) {
         let label = match self.oscillo_started {
             true => "Stop",
@@ -113,7 +192,7 @@ impl Application {
 
         let toggle = ::conrod::widget::Toggle::new(self.oscillo_started)
             .w_h(100.0, 50.0)
-            .mid_right_of(ids.canvas)
+            .middle_of(ids.side_panel)
             .color(self.bg_color.plain_contrast())
             .label(label)
             .label_color(self.bg_color)
@@ -148,19 +227,5 @@ impl Application {
 
             self.generator_started = value;
         }
-    }
-
-    fn draw_data<I>(&mut self, data: I, ui: &mut ::conrod::UiCell, ids: &Ids) where I: Iterator<Item=f64> {
-        let data: Vec<f64> = data.collect();
-
-        let plot = ::conrod::widget::PlotPath::new(0, data.len() - 1, -2.0, 2.0, |x| {
-            return data[x];
-        });
-
-        plot.color(::conrod::color::LIGHT_BLUE)
-            .padded_w_of(ids.canvas, 100.0)
-            .padded_h_of(ids.canvas, 30.0)
-            .top_left_of(ids.canvas)
-            .set(ids.plot, ui);
     }
 }
