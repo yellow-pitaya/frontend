6cc827413b4f6a2827dc69e606261de9576dc55b
Sanpi <sanpi@homecomputing.fr>
2020-02-09 00:05:39 +0100

    Updates env_logger to 0.7

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,7 @@ struct Args
 }
 
 fn main() {
-    env_logger::init()
-        .unwrap();
+    env_logger::init();
 
     let docopt = match ::docopt::Docopt::new(USAGE) {
         Ok(docopt) => docopt,
